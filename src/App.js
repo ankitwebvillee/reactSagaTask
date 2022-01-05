4a53e7973c5e056dfefc8a0335368b040fc6dd12
ankitwebvillee <ankitgadewal.webvillee@gmail.com>
2022-01-05 13:45:02 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,81 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
-import Conversation from './pages/Conversation';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ApiTest from './pages/ApiTest';
-import SelectContact from './pages/SelectContact';
-import TypeTitle from './pages/TypeTitle';
-import YourConversation from './pages/YourConversation';
-import { notification } from 'antd';
+import Home from "./pages/Home";
+import Conversation from "./pages/Conversation";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ApiTest from "./pages/ApiTest";
+import SelectContact from "./pages/SelectContact";
+import TypeTitle from "./pages/TypeTitle";
+import YourConversation from "./pages/YourConversation";
+import { notification } from "antd";
 // import { CONNECT_SOCKET } from './actions/actionType';
 
 function App() {
-  const [socket, setSocket] = useState(null)
+  const [socket, setSocket] = useState(null);
   const dispatch = useDispatch();
   const { selectedUser } = useSelector((state) => state.selectedData);
-
+  console.log("hello");
 
   useEffect(() => {
     if (socket === null) {
       // dispatch({type: CONNECT_SOCKET})  //used saga for connecting sockets but getting infinite loop issue while dispatching the action and not have much time to resolve this issue so implemented sockets in App.js
-      connectSockets()
+      connectSockets();
     }
-  }, [socket])
+  }, [socket]);
 
   useEffect(() => {
-    if(socket !== null){
-      socket.onmessage = evt => {
-        const message = JSON.parse(evt.data)
-        if (message?.type !== 'welcome' && message?.type !== 'ping' && message?.type !== 'confirm_subscription') {
-          if (selectedUser?.id !== message?.message?.sender_id && message?.message?.contact_ids?.includes(selectedUser?.id)) {
+    if (socket !== null) {
+      socket.onmessage = (evt) => {
+        const message = JSON.parse(evt.data);
+        if (
+          message?.type !== "welcome" &&
+          message?.type !== "ping" &&
+          message?.type !== "confirm_subscription"
+        ) {
+          if (
+            selectedUser?.id !== message?.message?.sender_id &&
+            message?.message?.contact_ids?.includes(selectedUser?.id)
+          ) {
             // Chat Notifications will only show to users who are in contact ids and not to the user who send it
             notification.info({
               message: message?.message?.sender_name,
               description: message.message?.content,
               className: "chatNoti",
-              icon: false
+              icon: false,
             });
           }
 
           // Used to append new message with older messages
           dispatch({
             type: "NEW_MESSAGE",
-            payload: message?.message
-          })
+            payload: message?.message,
+          });
         }
-      }
+      };
     }
-  }, [selectedUser])
+  }, [selectedUser]);
 
   const connectSockets = () => {
-    const ws = new WebSocket(process.env.REACT_APP_SOCKET_URL) // connecting sockets
+    const ws = new WebSocket(process.env.REACT_APP_SOCKET_URL); // connecting sockets
 
     ws.onopen = () => {
-      console.log('connected')
-      setSocket(ws)
-      ws.send(JSON.stringify({
-        command: 'subscribe',
-        identifier: JSON.stringify({
-          channel: 'NotificationsChannel',
-        }),
-      }));
-    }
+      console.log("connected");
+      setSocket(ws);
+      ws.send(
+        JSON.stringify({
+          command: "subscribe",
+          identifier: JSON.stringify({
+            channel: "NotificationsChannel",
+          }),
+        })
+      );
+    };
 
     ws.onclose = () => {
-      console.log('disconnected')
-    }
-  }
+      console.log("disconnected");
+    };
+  };
 
   return (
     <BrowserRouter>
